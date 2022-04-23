3649ad0e7b355a903950c3015725bf72bbb7d5bc
kyagloski <kyleyagloski@gmail.com>
2022-04-23 02:09:03 -0400

    done for the night

--- src/main/java/net/fabricmc/example/BaritoneToolbox.java
+++ src/main/java/net/fabricmc/example/BaritoneToolbox.java
@@ -12,9 +12,6 @@ public class BaritoneToolbox implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 		ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
 			((InGameHudAccess)client.inGameHud).registerChatListener(
 					MessageType.CHAT,
--- src/main/java/net/fabricmc/example/Commands.java
+++ src/main/java/net/fabricmc/example/Commands.java
@@ -5,17 +5,23 @@ import net.minecraft.client.MinecraftClient;
 public class Commands {
 
     private static final MinecraftClient mc = MinecraftClient.getInstance();
+    private static int cooldown = 0;
 
     public static void about() {
+        // prints about text
+        // currently can be exploited to get bot kicked (spam)
+        // TODO: implement cooldown or input spam filter
         String[] aboutTexts = { "+--------BARITONE-UTILS--------+",
                 "+-adds global chat commands------+",
-                "+-for remote baritone bots-------+",
-                "+-created by: github.com/kyagloski/" };
+                "+-for remote baritone bots -------+",
+                "+-created by: github.com/kyagloski -+",
+                "+------------------------------+"};
         for (String str : aboutTexts) {
             mc.player.sendChatMessage(str);
             try { Thread.sleep(100); }
             catch(Exception e) { return; } // this is good practice, trust me
         }
+
     }
 
     public static void help() {
